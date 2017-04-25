8af74bfaec34dca55b8afb3cea231fdfc707fb71
Wang <654917930@qq.com>
2017-04-25 17:18:54 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by wang on 2017/4/21.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+        public String pm25;
+
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,23 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by wang on 2017/4/21.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+
+        @SerializedName("txt")
+        public String info;
+    }
+
+}
